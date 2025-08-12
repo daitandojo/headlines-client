65431716e3635c6a4db99862375009db3679defa
Daitan Dojo <haelpers@gmail.com>
2025-08-12 22:59:19 +0100

    Forgot push model, now included.

---
+++ src/models/PushSubscription.js
@@ -0,0 +1,20 @@
+// models/PushSubscription.js (version 1.0)
+import mongoose from 'mongoose';
+
+const { Schema, model, models } = mongoose;
+
+const PushSubscriptionSchema = new Schema(
+  {
+    endpoint: { type: String, required: true, unique: true },
+    keys: {
+      p256dh: { type: String, required: true },
+      auth: { type: String, required: true },
+    },
+  },
+  {
+    timestamps: true,
+    collection: 'push_subscriptions',
+  }
+);
+
+export default models.PushSubscription || model('PushSubscription', PushSubscriptionSchema);
